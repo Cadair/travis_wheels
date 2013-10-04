85cbf5304d6185e050556f92cc62285630d9f9e4
Stuart Mumford <stuart@mumford.me.uk>
2013-10-04 16:07:01 +0100

    Add exit status aggregation to the shell scripts

--- make_core_wheels.sh
+++ make_core_wheels.sh
@@ -1,7 +1,11 @@
-pip wheel --wheel-dir=./wheelhouse numpy==$NUMPY_VERSION_2;
-pip wheel --wheel-dir=./wheelhouse matplotlib==$MATPLOTLIB_VERSION;
+# Aggregate the exit status so build fails if a wheel is not created:
+EXIT_STATUS=0
+pip wheel --wheel-dir=./wheelhouse numpy==$NUMPY_VERSION_2 || EXIT_STATUS=$?
+pip wheel --wheel-dir=./wheelhouse matplotlib==$MATPLOTLIB_VERSION || EXIT_STATUS=$?
 # Manually make pandas deps due to pip 1.4 / pytz versioning bug
-pip wheel --wheel-dir=./wheelhouse pytz==2013b;
-pip wheel --wheel-dir=./wheelhouse python-dateutil;
-pip wheel --wheel-dir=./wheelhouse --no-deps pandas==$PANDAS_VERSION_1;
-pip wheel --wheel-dir=./wheelhouse --no-deps pandas==$PANDAS_VERSION_2;
+pip wheel --wheel-dir=./wheelhouse pytz==2013b || EXIT_STATUS=$?
+pip wheel --wheel-dir=./wheelhouse python-dateutil || EXIT_STATUS=$?
+pip wheel --wheel-dir=./wheelhouse --no-deps pandas==$PANDAS_VERSION_1 || EXIT_STATUS=$?
+pip wheel --wheel-dir=./wheelhouse --no-deps pandas==$PANDAS_VERSION_2 || EXIT_STATUS=$?
+
+exit $EXIT_STATUS
--- make_extra_wheels.sh
+++ make_extra_wheels.sh
@@ -1,10 +1,14 @@
-pip wheel --wheel-dir=./wheelhouse --no-deps astropy;
-pip wheel --wheel-dir=./wheelhouse $SUDS_PKG;
-pip wheel --wheel-dir=./wheelhouse beautifulsoup4;
-pip wheel --wheel-dir=./wheelhouse pytest pytest-xdist;
-pip wheel --wheel-dir=./wheelhouse h5py;
-pip wheel --wheel-dir=./wheelhouse sphinx;
+# Aggregate the exit status so build fails if a wheel is not created:
+EXIT_STATUS=0
+pip wheel --wheel-dir=./wheelhouse --no-deps astropy || EXIT_STATUS=$?
+pip wheel --wheel-dir=./wheelhouse $SUDS_PKG || EXIT_STATUS=$?
+pip wheel --wheel-dir=./wheelhouse beautifulsoup4 || EXIT_STATUS=$?
+pip wheel --wheel-dir=./wheelhouse pytest pytest-xdist || EXIT_STATUS=$?
+pip wheel --wheel-dir=./wheelhouse h5py || EXIT_STATUS=$?
+pip wheel --wheel-dir=./wheelhouse sphinx || EXIT_STATUS=$?
 #Make glymur deps manually to prevent another numpy build
-pip wheel --wheel-dir=./wheelhouse contextlib2;
-pip wheel --wheel-dir=./wheelhouse mock;
-pip wheel --wheel-dir=./wheelhouse --no-deps glymur;
+pip wheel --wheel-dir=./wheelhouse contextlib2 || EXIT_STATUS=$?
+pip wheel --wheel-dir=./wheelhouse mock || EXIT_STATUS=$?
+pip wheel --wheel-dir=./wheelhouse --no-deps glymur || EXIT_STATUS=$?
+
+exit $EXIT_STATUS
