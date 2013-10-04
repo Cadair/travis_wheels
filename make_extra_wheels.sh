9cce192ff572585c2d17481325bbdc0d873c2d52
Stuart Mumford <stuart@mumford.me.uk>
2013-10-04 15:57:38 +0100

    manual glymur deps to prevent numpu build

--- make_extra_wheels.sh
+++ make_extra_wheels.sh
@@ -4,4 +4,7 @@ pip wheel --wheel-dir=./wheelhouse beautifulsoup4;
 pip wheel --wheel-dir=./wheelhouse pytest pytest-xdist;
 pip wheel --wheel-dir=./wheelhouse h5py;
 pip wheel --wheel-dir=./wheelhouse sphinx;
-pip wheel --wheel-dir=./wheelhouse glymur;
+#Make glymur deps manually to prevent another numpy build
+pip wheel --wheel-dir=./wheelhouse contextlib2;
+pip wheel --wheel-dir=./wheelhouse mock;
+pip wheel --wheel-dir=./wheelhouse --no-deps glymur;
