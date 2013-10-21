a98f97ab5845f7a00c5aef892f7644e28f4fd6de
Stuart Mumford <stuart@mumford.me.uk>
2013-10-21 10:13:56 +0100

    change to latest mpl

--- make_core_wheels.sh
+++ make_core_wheels.sh
@@ -1,7 +1,7 @@
 # Aggregate the exit status so build fails if a wheel is not created:
 EXIT_STATUS=0
 pip wheel --wheel-dir=./wheelhouse numpy==$NUMPY_VERSION_2 || EXIT_STATUS=$?
-pip wheel --wheel-dir=./wheelhouse matplotlib==$MATPLOTLIB_VERSION || EXIT_STATUS=$?
+pip wheel --wheel-dir=./wheelhouse matplotlib || EXIT_STATUS=$?
 # Manually make pandas deps due to pip 1.4 / pytz versioning bug
 pip wheel --wheel-dir=./wheelhouse pytz==2013b || EXIT_STATUS=$?
 pip wheel --wheel-dir=./wheelhouse python-dateutil || EXIT_STATUS=$?
