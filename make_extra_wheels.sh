cd6aed561eea744f67e28522546df27ee89d3d54
Stuart Mumford <stuart@mumford.me.uk>
2014-01-14 10:52:26 +0000

    allow external h5py

--- make_extra_wheels.sh
+++ make_extra_wheels.sh
@@ -4,7 +4,7 @@ pip wheel --wheel-dir=./wheelhouse --no-deps astropy || EXIT_STATUS=$?
 pip wheel --wheel-dir=./wheelhouse $SUDS_PKG || EXIT_STATUS=$?
 pip wheel --wheel-dir=./wheelhouse beautifulsoup4 || EXIT_STATUS=$?
 pip wheel --wheel-dir=./wheelhouse pytest pytest-xdist || EXIT_STATUS=$?
-pip wheel --wheel-dir=./wheelhouse h5py || EXIT_STATUS=$?
+pip wheel --wheel-dir=./wheelhouse --allow-external h5py || EXIT_STATUS=$?
 pip wheel --wheel-dir=./wheelhouse sphinx || EXIT_STATUS=$?
 #Make glymur deps manually to prevent another numpy build
 pip wheel --wheel-dir=./wheelhouse contextlib2 || EXIT_STATUS=$?
