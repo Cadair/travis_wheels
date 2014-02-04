4a297f51c05d7551344c966a72794cca0f1a617e
Stuart Mumford <stuart@mumford.me.uk>
2014-02-04 12:08:23 +0000

    build latest pandas version and latest suds-jurko

--- make_core_wheels.sh
+++ make_core_wheels.sh
@@ -6,6 +6,6 @@ pip wheel --wheel-dir=./wheelhouse matplotlib || EXIT_STATUS=$?
 pip wheel --wheel-dir=./wheelhouse pytz==2013b || EXIT_STATUS=$?
 pip wheel --wheel-dir=./wheelhouse python-dateutil || EXIT_STATUS=$?
 pip wheel --wheel-dir=./wheelhouse --no-deps pandas==$PANDAS_VERSION_1 || EXIT_STATUS=$?
-pip wheel --wheel-dir=./wheelhouse --no-deps pandas==$PANDAS_VERSION_2 || EXIT_STATUS=$?
+pip wheel --wheel-dir=./wheelhouse --no-deps pandas || EXIT_STATUS=$?
 
 exit $EXIT_STATUS
