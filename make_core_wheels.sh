652ba48a9f2733d40dd3be0194a8da94051f78c0
Stuart Mumford <stuart@mumford.me.uk>
2014-12-23 15:46:10 +0000

    Now astropy are no longer using this, reduce to SunPy requirements only.

--- make_core_wheels.sh
+++ make_core_wheels.sh
@@ -2,10 +2,7 @@
 EXIT_STATUS=0
 pip wheel --wheel-dir=./wheelhouse numpy==$NUMPY_VERSION_2 || EXIT_STATUS=$?
 pip wheel --wheel-dir=./wheelhouse matplotlib || EXIT_STATUS=$?
-# Manually make pandas deps due to pip 1.4 / pytz versioning bug
-pip wheel --wheel-dir=./wheelhouse pytz==2013b || EXIT_STATUS=$?
-pip wheel --wheel-dir=./wheelhouse python-dateutil || EXIT_STATUS=$?
-pip wheel --wheel-dir=./wheelhouse --no-deps pandas==$PANDAS_VERSION_1 || EXIT_STATUS=$?
-pip wheel --wheel-dir=./wheelhouse --no-deps pandas || EXIT_STATUS=$?
+pip wheel --wheel-dir=./wheelhouse pandas==$PANDAS_VERSION_1 || EXIT_STATUS=$?
+pip wheel --wheel-dir=./wheelhouse pandas || EXIT_STATUS=$?
 
 exit $EXIT_STATUS
--- make_extra_wheels.sh
+++ make_extra_wheels.sh
@@ -1,10 +1,9 @@
 # Aggregate the exit status so build fails if a wheel is not created:
 EXIT_STATUS=0
 pip wheel --wheel-dir=./wheelhouse --no-deps astropy || EXIT_STATUS=$?
-pip wheel --wheel-dir=./wheelhouse $SUDS_PKG || EXIT_STATUS=$?
+pip wheel --wheel-dir=./wheelhouse suds-jurko || EXIT_STATUS=$?
 pip wheel --wheel-dir=./wheelhouse beautifulsoup4 || EXIT_STATUS=$?
 pip wheel --wheel-dir=./wheelhouse pytest pytest-xdist || EXIT_STATUS=$?
-pip wheel --wheel-dir=./wheelhouse --allow-external h5py || EXIT_STATUS=$?
 pip wheel --wheel-dir=./wheelhouse sphinx || EXIT_STATUS=$?
 #Make glymur deps manually to prevent another numpy build
 pip wheel --wheel-dir=./wheelhouse contextlib2 || EXIT_STATUS=$?
