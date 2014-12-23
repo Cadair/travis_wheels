dd6c1fbef8af57861855cf7e3c668f35b5f48c17
Stuart Mumford <stuart@mumford.me.uk>
2014-12-23 16:08:31 +0000

    More simplifcation, and hopefully less numpy.

--- make_core_wheels.sh
+++ make_core_wheels.sh
@@ -1,8 +1,7 @@
 # Aggregate the exit status so build fails if a wheel is not created:
 EXIT_STATUS=0
+pip wheel --wheel-dir=./wheelhouse matplotlib pandas || EXIT_STATUS=$?
 pip wheel --wheel-dir=./wheelhouse numpy==$NUMPY_VERSION_2 || EXIT_STATUS=$?
-pip wheel --wheel-dir=./wheelhouse matplotlib || EXIT_STATUS=$?
 pip wheel --wheel-dir=./wheelhouse pandas==$PANDAS_VERSION_1 || EXIT_STATUS=$?
-pip wheel --wheel-dir=./wheelhouse pandas || EXIT_STATUS=$?
 
 exit $EXIT_STATUS
