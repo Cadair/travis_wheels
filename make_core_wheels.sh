690846ae8fb475b2f967b6fbe70dfe1f2ce52625
Stuart Mumford <stuart@mumford.me.uk>
2015-02-24 14:39:29 +0000

    Also add a most recent pandas + numpy

--- make_core_wheels.sh
+++ make_core_wheels.sh
@@ -3,5 +3,7 @@ EXIT_STATUS=0
 pip wheel --wheel-dir=./wheelhouse matplotlib pandas || EXIT_STATUS=$?
 pip wheel --wheel-dir=./wheelhouse numpy==$NUMPY_VERSION_1 || EXIT_STATUS=$?
 pip wheel --wheel-dir=./wheelhouse pandas==$PANDAS_VERSION_1 || EXIT_STATUS=$?
+pip wheel --wheel-dir=./wheelhouse --no-deps pandas || EXIT_STATUS=$?
+pip wheel --wheel-dir=./wheelhouse --no-deps numpy || EXIT_STATUS=$?
 
 exit $EXIT_STATUS
